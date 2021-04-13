0455768bbac7b98e2b7e62c20b08ffcb315ea701
Rashad Bayram <rashadabayram.com>
2021-04-13 15:05:50 -0600

    added delete post router to server and delete button to index

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; 
+const PORT = 8080;
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -41,15 +41,21 @@ app.get("/urls/:shortURL", (req, res) => {
 app.post("/urls", (req, res) => {
   let shortUrl = generateRandomString();
   urlDatabase[shortUrl] = req.body.longURL;
-  res.redirect(`/urls/${shortUrl}`);        
+  res.redirect(`/urls/${shortUrl}`);
 });
 
 const generateRandomString = () => {
   let randomString = Math.random().toString(36).substring(6);
   return randomString;
-}
+};
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const urlToDelete = req.params.shortURL;
+  delete urlDatabase[urlToDelete];
+  res.redirect("/urls");
 });
