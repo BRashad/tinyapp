1b16f1563034bd02df12d06c236964e6b2d292db
Rashad Bayram <rashadabayram.com>
2021-04-16 15:55:02 -0600

    code clean up

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,31 +4,31 @@ const { emailLookUp } = require('../helper');
 
 const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = emailLookUp("user@example.com", users)
+    const user = emailLookUp("user@example.com", users);
     const expectedOutput = "userRandomID";
 
     assert.equal(user.id, expectedOutput);
     
   });
   it('should return undefined', function() {
-    const user = emailLookUp("", users)
+    const user = emailLookUp("", users);
     const expectedOutput = undefined;
-
-    assert.equal(user.id, expectedOutput); 
+    
+    assert.equal(user.id, expectedOutput);
     
   });
-}); 
+});
 
